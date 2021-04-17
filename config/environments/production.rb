3a5f280f46643dbdcaa746290b070f5eca9550cd
masudagakuto <tomatomato5321@gmail.com>
2021-04-17 14:47:27 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,6 +111,6 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  # config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = :uglifier
 
 end
